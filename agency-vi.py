3597057ee8057f72e4b6e6940868ac67b690be5f
TechJustice101 <138499851+Huylorddd@users.noreply.github.com>
2025-05-15 02:55:37 +0700

    Update agency-vi.py

--- agency-vi.py
+++ agency-vi.py
@@ -56,8 +56,8 @@ while True:
     with open("voice.mp3", "wb") as file:
       file.write(audio_response.content)
     pygame.mixer.init()
+    
     try:
-        
         pygame.mixer.music.load("voice.mp3")
         pygame.mixer.music.play()
         while pygame.mixer.music.get_busy():
